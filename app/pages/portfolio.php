e92bdb30b4d2714024c8f2b77647c369095555ab
Alexandr Kuzmenko <ak@nullgr.com>
2016-07-10 18:56:21 +0300

    Сделал вывод всего текста на страницу портфолио из базы

--- app/pages/portfolio.php
+++ app/pages/portfolio.php
@@ -1,225 +1,190 @@
-<h1>Портфолио</h1>
+<?php
+
+require_once 'scripts/connect.php';
+
+$lang      = $_SESSION['lang'];
+$get_boxes = mysql_query("SELECT type, position, text_{$lang} FROM portfolio_page");
+$i         = 0; // 'id' and 'for' number of checkboxes (should be incremented in cycle)
+
+while ($box = mysql_fetch_array($get_boxes)) {
+	switch ($box['type']) {
+
+		case 'checkbox':
+			switch ($box['position']) {
+				case 'top_box':
+					$top_box[]    = $box[2];
+					break;
+				case 'middle_box':
+					$middle_box[] = $box[2];
+					break;
+				case 'bottom_box':
+					$bottom_box[] = $box[2];
+					break;
+			}
+			break;
+		
+		case 'header':
+			switch ($box['position']) {
+				case 'header':
+					$head           = $box[2];
+					break;
+				case 'top_box':
+					$top_head       = $box[2];
+					break;
+				case 'middle_box':
+					$middle_head    = $box[2];
+					break;
+				case 'bottom_box':
+					$bottom_head    = $box[2];
+					break;
+				case 'random-portfolio':
+					$portfolio_head = $box[2];
+					break;
+			}
+			break;
+		
+		case 'link':
+			switch ($box['position']) {
+				case 'random-portfolio':
+					$portfolio_link = $box[2];
+					break;
+				case 'button':
+					$display = $box[2];
+					break;
+			}
+			break;
+		
+		case 'text':
+			switch ($box['position']) {
+				case 'top':
+					$top_text = $box[2];
+					break;
+				case 'bottom':
+					$bottom_text = $box[2];
+					break;
+			}
+			break;
+		
+		case 'indicator':
+			switch ($box['position']) {
+				case 'top_box':
+					$no_choosen = $box[2];
+					break;
+			}
+			break;
+	}
+}
+
+?>
+
+<h1><?= $head ?></h1>
 
 <div class="checkbox-header">
-	<h2>Вид услуги:</h2>
-	<span>Не выбрано</span>
+	<h2><?= $top_head ?></h2>
+	<span><?= $no_choosen ?></span>
 </div>
 <div class="checkbox-wrapper">
-	<div>
-		<input type="checkbox" id="option-1">
-		<label for="option-1">Копирайтинг</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-2">
-		<label for="option-2">SEO - копирайтинг</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-3">
-		<label for="option-3">Копирайтинг для главной страницы</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-4">
-		<label for="option-4">Копирайтинг на английском</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-5">
-		<label for="option-5">Рерайтинг</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-6">
-		<label for="option-6">SEO-Рерайтинг</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-7">
-		<label for="option-7">Рерайтинг на английском</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-8">
-		<label for="option-8">Создание семантического ядра</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-9">
-		<label for="option-9">Перевод (русский, английский, немецкий и т.д.)</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-10">
-		<label for="option-10">Рекламные тексты</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-11">
-		<label for="option-11">Комменческое предложение</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-12">
-		<label for="option-12">Landing Page (Лендинг)</label>
-	</div>
-</div>
+	<?php
 
-<div class="checkbox-header">
-	<h2>Тематическая категория:</h2>
-</div>
-<div class="checkbox-wrapper">
-	<div>
-		<input type="checkbox" id="option-13">
-		<label for="option-13">
-			Бизнес <span>(трудоустройство, маркетинг, менеджмент, реклама, компании, товары и услуги)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-14">
-		<label for="option-14">
-			Бухгалтерия и фининсы <span>(бухгалтерия, банки, кредитование, инвестиции, платежные системы)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-15">
-		<label for="option-15">
-			Закон и право <span>(законодательство, страхование, юридические услуги, криминалистика)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-16">
-		<label for="option-16">
-			Инетрент <span>(софт, заработок, создание сайтов, продвижение, безопасность, хостинги/домены)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-17">
-		<label for="option-17">
-			Красота и здоровье <span>(косметика, салон, ЗОЖ, диеты/питание, фитнес, массаж, баня)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-18">
-		<label for="option-18">
-			Кулинария <span>(национальные кухни, диеты, продукты, рецепты, сервисы и сайты)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-19">
-		<label for="option-19">
-			Культура <span>(кино, музыка, фото, литература, театры, живопись)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-20">
-		<label for="option-20">
-			Медицина <span>(психология, нетрадиционная, пластика, фарма, беременность, ветеринария, стомат)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-21">
-		<label for="option-21">
-			Мода <span>(одежда, аксессуары, тенденции, показ мод)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-22">
-		<label for="option-22">
-			Наука и образование <span>(виды науки, обучение, высокие технологии, философия, воспитание)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-23">
-		<label for="option-23">
-			Недвижимость <span>(аренда, продажа, коттеджи, квартиры, офисы, ТЦ, рестораны, места отдыха)</span>
-		</label>
-	</div>
-</div>
+	foreach ($top_box as $value) {
+		echo <<<HTML
+			<div>
+				<input type="checkbox" id="option-{$i}">
+				<label for="option-{$i}">
+					{$value}
+				</label>
+			</div>
+HTML;
+		$i++;
+	}
 
+	?>
+</div>
 <div class="checkbox-header">
-	<h2>Ветка исполнителей:</h2>
+	<h2><?= $middle_head ?></h2>
 </div>
 <div class="checkbox-wrapper">
-	<div>
-		<input type="checkbox" id="option-24">
-		<label for="option-24">
-			Специалист <span>(узнать подробнее)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-25">
-		<label for="option-25">
-			Опытный <span>(узнать подробнее)</span>
-		</label>
-	</div>
-	<div>
-		<input type="checkbox" id="option-26">
-		<label for="option-26">
-			VIP <span>(узнать подробнее)</span>
-		</label>
-	</div>
+	<?php
+
+	foreach ($middle_box as $value) {
+		echo <<<HTML
+			<div>
+				<input type="checkbox" id="option-{$i}">
+				<label for="option-{$i}">
+					{$value}
+				</label>
+			</div>
+HTML;
+		$i++;
+	}
+
+	?>
+</div>
+<div class="checkbox-header">
+	<h2><?= $bottom_head ?></h2>
+</div>
+<div class="checkbox-wrapper">
+	<?php
+
+	foreach ($bottom_box as $value) {
+		echo <<<HTML
+			<div>
+				<input type="checkbox" id="option-{$i}">
+				<label for="option-{$i}">
+					{$value}
+				</label>
+			</div>
+HTML;
+		$i++;
+	}
+
+	?>
 </div>
 
 <div class="render-portfolio">
-	<div class="btn">Отобразить</div>
+	<div class="btn" id="render-portfolio"><?= $display ?></div> <!-- ZALUPA -->
 </div>
 
 <div class="trigger" data-arrow="random-portfolio">
-	<h2><img src="img/ico/minus_black.svg" alt="minus"> <span>Случайные работы из портфолио</span></h2>
+	<h2><img src="img/ico/plus_black.svg" alt="minus"> <span><?= $portfolio_head ?></span></h2>
 	<img src="img/ico/reload.svg" alt="reload" class="reload">
 </div>
 <div class="random-portfolio" data-target="random-portfolio">
 	<div class="tags">
-		<div>Копирайтинг</div>
-		<div>Перевод</div>
-		<div>Рекламные тексты</div>
-		<div>Лендинг</div>
-		<div>Пресс-релиз</div>
-		<div>Описание товаров</div>
-		<div>Нейминги</div>
-		<div>Слоганы</div>
-		<div>Подбор доменного имени</div>
-		<div>Сценарий к видеоролику</div>
-		<div>Речь, спичрайтинг, рапорт</div>
-		<div>Стихотворения</div>
-		<div>Реферат</div>
-		<div>Курсовая</div>
-		<div>Дипломная</div>
+		<?php
+
+		$get_tags = mysql_query("SELECT {$lang} FROM tags");
+
+		while ($tag = mysql_fetch_array($get_tags)) {
+			echo '<div>' . $tag[0] . '</div>';
+		}
+
+		?>
 	</div>
 	<div class="wrapper">
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
-		<div class="item">
-			<div class="name">Пример из портфолио</div>
-			<img src="img/temp/portfolio.jpg" alt="portfolio">
-		</div>
+		<?php
+
+		$get_portfolio = mysql_query("SELECT category, name_{$lang}, photo FROM portfolio ORDER BY RAND() LIMIT 8");
+
+		while ($portfolio = mysql_fetch_array($get_portfolio)) {
+
+			$name  = $portfolio[1];
+			$photo = $portfolio['photo'];
+
+			echo <<<HTML
+				<div class="item">
+					<div class="name">{$name}</div>
+					<img src="img/portfolio/{$photo}">
+				</div>
+HTML;
+		}
+
+		?>
 	</div>
 	<div class="all-portfolio">
-		<a href="#">Всё портфолио...</a>
+		<a href="#"><?= $portfolio_link ?></a>
 	</div>
 </div>
 
-<p>
-	Мы один из старейших производителей письменного дела на просторах Интернет, а на рынке копирайтинга существуем с 2005-го. За последний год было выполнено свыше 100 000 разнопрофильных заданий. Здесь всего лишь “капля в море” того, чем мы можем похвастаться. Если вы не обнаружили нужный пример из портфолио, пишите, поищем в архиве.
-</p>
-
-<p>
-	Мы один из старейших производителей письменного дела на просторах Интернет, а на рынке копирайтинга существуем с 2005-го. За последний год было выполнено свыше 100 000 разнопрофильных заданий. Здесь всего лишь “капля в море” того, чем мы можем похвастаться. Если вы не обнаружили нужный пример из портфолио, пишите, поищем в архиве. Мы один из старейших производителей письменного дела на просторах Интернет, а на рынке копирайтинга существуем с 2005-го. За последний год было выполнено свыше 100 000 разнопрофильных заданий. Здесь всего лишь “капля в море” того, чем мы можем похвастаться. Если вы не обнаружили нужный пример из портфолио, пишите, поищем в архиве.
-</p>
+<?= $top_text ?>
+<?= $bottom_text ?>
